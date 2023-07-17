900f77a1da82d819e3ab30f34ef117b0875d3a06
João José Cardoso Ribeiro <joaojosecr@gmail.com>
2023-07-17 18:25:59 -0300

    att

--- src/elasticsearch.js
+++ src/elasticsearch.js
@@ -14,16 +14,77 @@ exports.getClient = async(req, res) =>{
 
 exports.insert = async(req, res) =>{
 
-
-    const result = await client.index({
-        index: 'produto',
-        type: 'type_produto',
-        body: {
-            nome: 'Coca Cola lata zero 350ml',
-            supermercado: 'dale',
-            preco: 5
+    const products = [
+        {
+          nome: 'Coca Cola lata zero 350ml',
+          supermercado: 'dale',
+          preco: 5
         },
-    })
+        {
+          nome: 'Pepsi lata zero 350ml',
+          supermercado: 'dale',
+          preco: 4
+        },
+        {
+          nome: 'Água mineral 500ml',
+          supermercado: 'dale',
+          preco: 1.5
+        },
+        {
+          nome: 'Biscoito recheado',
+          supermercado: 'dale',
+          preco: 2.5
+        },
+        {
+          nome: 'Leite integral 1L',
+          supermercado: 'dale',
+          preco: 3.2
+        },
+        {
+          nome: 'Sabonete líquido',
+          supermercado: 'dale',
+          preco: 6.9
+        },
+        {
+          nome: 'Detergente 500ml',
+          supermercado: 'dale',
+          preco: 2.3
+        },
+        {
+          nome: 'Arroz branco 1kg',
+          supermercado: 'dale',
+          preco: 4.5
+        },
+        {
+          nome: 'Feijão carioca 1kg',
+          supermercado: 'dale',
+          preco: 3.8
+        },
+        {
+          nome: 'Macarrão espaguete',
+          supermercado: 'dale',
+          preco: 2.1
+        }
+      ];
+      
+      for (const product of products) {
+        await client.index({
+          index: 'produto',
+          type: 'type_produto',
+          body: product,
+        });
+      }
+      
+    // const result = await client.index({
+    //     index: 'produto',
+    //     type: 'type_produto',
+    //     body: {
+    //         nome: 'Coca Cola lata zero 350ml',
+    //         supermercado: 'dale',
+    //         preco: 5
+    //     },
+        
+    // })
 
     return res.status(200).send({
         message: result,
@@ -40,9 +101,9 @@ exports.findAll = async(req, res) =>{
             query: {
                 match : {
                     nome : {
-                        query : "Coca Cola 350ml",
+                        query : "Pepsi 350ml",
                         fuzziness: 1,
-                        transpositions: true, 
+
                         operator: "and"
                     }
                 }
